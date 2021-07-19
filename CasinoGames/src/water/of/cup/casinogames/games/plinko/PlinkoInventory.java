6b40b5ccba48fea5f780e1fe00af3a0efac07367
Achoowy <Achoowy@gmail.com>
2021-07-19 01:05:15 -0500

    Functional Plinko added

---
+++ CasinoGames/src/water/of/cup/casinogames/games/plinko/PlinkoInventory.java
@@ -0,0 +1,80 @@
+package water.of.cup.casinogames.games.plinko;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+import org.bukkit.Material;
+
+import water.of.cup.boardgames.game.GamePlayer;
+import water.of.cup.boardgames.game.inventories.GameInventory;
+import water.of.cup.boardgames.game.inventories.GameOption;
+import water.of.cup.boardgames.game.inventories.GameOptionType;
+import water.of.cup.casinogames.games.slots.SlotsGame;
+
+public class PlinkoInventory extends GameInventory {
+
+	private final Plinko game;
+
+	public PlinkoInventory(Plinko game) {
+		super(game);
+		this.game = game;
+	}
+
+	@Override
+	protected ArrayList<GameOption> getOptions() {
+		ArrayList<GameOption> options = new ArrayList<GameOption>();
+		GameOption bet = new GameOption("betAmount", Material.GOLD_NUGGET, GameOptionType.COUNT, null, "0", true);
+		options.add(bet);
+
+		List<String> riskValues = Arrays.asList("Low Risk", "Normal Risk", "High Risk");
+		GameOption risk = new GameOption("risk", Material.EXPERIENCE_BOTTLE, GameOptionType.COUNT, null, riskValues.get(1), riskValues);
+		options.add(risk);
+		return options;
+	}
+
+	@Override
+	protected int getMaxQueue() {
+		return 0;
+	}
+
+	@Override
+	protected int getMaxGame() {
+		return 1;
+	}
+
+	@Override
+	protected int getMinGame() {
+		return 1;
+	}
+
+	@Override
+	protected boolean hasTeamSelect() {
+		return false;
+	}
+
+	@Override
+	protected boolean hasGameWagers() {
+		return false;
+	}
+
+	@Override
+	protected boolean hasWagerScreen() {
+		return false;
+	}
+
+	@Override
+	protected boolean hasForfeitScreen() {
+		return false;
+	}
+
+	@Override
+	protected void onGameCreate(HashMap<String, Object> gameData, ArrayList<GamePlayer> players) {
+//        for(GamePlayer player : players) {
+//            player.getPlayer().sendMessage(ConfigUtil.CHAT_WELCOME_GAME.buildString("Slots"));
+//        }
+
+		game.startGame();
+	}
+}
